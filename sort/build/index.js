3e2698cb55338365aef443167842550d9034a539
Chung Byung Jae <jbj828@naver.com>
2021-08-31 00:42:05 +0900

    study typescript

---
+++ sort/build/index.js
@@ -0,0 +1,4 @@
+"use strict";
+var sorter = new Sorter([12, 3, 3, 32]);
+sorter.sort();
+console.log(sorter.collection);
---
+++ sort/src/index.ts
@@ -0,0 +1,3 @@
+const sorter = new Sorter([12, 3, 3, 32]);
+sorter.sort();
+console.log(sorter.collection);
