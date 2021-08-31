943e1fbab550ac513ab152f33ed89c331164ac3b
Chung Byung Jae <jbj828@naver.com>
2021-08-31 23:40:25 +0900

    study typescript

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -1,19 +1,20 @@
+export interface Sortable {
+  length: number;
+  swap(leftIndex: number, rightIndex: number): void;
+  compare(leftIndex: number, rightIndex: number): boolean;
+}
+
 export class Sorter {
-  constructor(public collection: /* TODO: fix me */) {}
+  constructor(public collection: Sortable) {}
 
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-       
-       
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-       
+        if (this.collection.compare(j, j + 1)) {
+          this.collection.swap(j, j + 1);
+        }
       }
     }
   }
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,3 +1,24 @@
-const sorter = new Sorter([12, 3, 3, 32]);
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
+
+// const numbersCollection = new NumbersCollection([4, 1, 3]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+// const charactersCollection = new CharactersCollection('Hello');
+// const stringSorter = new Sorter(charactersCollection);
+// stringSorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(3);
+linkedList.add(2);
+linkedList.add(50);
+linkedList.add(1);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(sorter.collection);
+console.log(linkedList.print());
