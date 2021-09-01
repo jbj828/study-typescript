9fe67965051eb082e830afd660cd651652ec8d84
Chung Byung Jae <jbj828@naver.com>
2021-09-01 22:33:29 +0900

    study typescript

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,24 +1,19 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([4, 1, 3]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([4, 1, 3]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection('Hello');
-// const stringSorter = new Sorter(charactersCollection);
-// stringSorter.sort();
-// console.log(charactersCollection.data);
+const charactersCollection = new CharactersCollection('sdfsa');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(3);
 linkedList.add(2);
 linkedList.add(50);
 linkedList.add(1);
-
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 console.log(linkedList.print());
