659a6da4a53048fb37556d295ec8770666a2b244
Chung Byung Jae <jbj828@naver.com>
2021-09-06 23:44:57 +0900

    study typescript

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,16 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'jay', age: 20 });
+const user = new User({ name: 'new record', age: 0 });
 
-user.on('change', () => {
-  console.log('change!!');
-});
-user.on('change', () => {
-  console.log('change2!!');
-});
-user.on('click', () => {
-  console.log('click!!');
-});
-
-user.trigger('change');
-user.trigger('sadfas');
+user.on('change', () => {});
