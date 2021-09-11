c49bead8aead157303ad280033c318c8c3018bf1
Chung Byung Jae <jbj828@naver.com>
2021-09-12 00:46:43 +0900

    study typescript

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,18 @@
-console.log('hi');
+import cookieSession from 'cookie-session';
+import express from 'express';
+import { router } from './routes/loginRoutes';
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cookieSession({ keys: ['salfksdf'] }));
+app.use(router);
+
+app.listen(3001, () => {
+  console.log('Listening on port 3001');
+});
